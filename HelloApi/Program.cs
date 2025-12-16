59a1e81ff57a57c85b579a2b92b847e99009e2aa
AhmedOkesina <ahmedokesina@yahoo.com>
2025-12-16 16:34:01 -0600

    Test PR pipeline

--- HelloApi/Program.cs
+++ HelloApi/Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/hello", () => Results.Ok(new { message = "Hello World!" }));
+app.MapGet("/hello", () => Results.Ok(new { message = "Hello Bro!" }));
 
 app.Run();
